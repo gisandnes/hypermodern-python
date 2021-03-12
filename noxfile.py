60374a302b20ed572e83987eed1feb42b6376dae
Geir Inge Sandnes <geir-inge.sandnes@gjensidige.no>
2021-03-12 13:15:50 +0100

    Git hooks with pre-commit

--- noxfile.py
+++ noxfile.py
@@ -7,21 +7,38 @@ nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--without-hashes",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8",
-                    "flake8-black",
-                    "flake8-import-order",
-                    "flake8-bugbear",
-                    "flake8-bandit")
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-black",
+        "flake8-import-order",
+        "flake8-bugbear",
+        "flake8-bandit",
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -37,12 +54,15 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
